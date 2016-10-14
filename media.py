93c388a82b4f3206a1a9be3a6746886f09f0ea18
Emily Le <emilyle265@gmail.com>
2016-10-14 12:07:05 -0700

    Add files via upload

---
+++ media.py
@@ -0,0 +1,8 @@
+class Movie():
+    def __init__(self, title, poster_image, trailer, director, actors, release):
+        self.title = title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer
+        self.director = director
+        self.actors = actors
+        self.release_date = release
