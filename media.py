27f254abc3961e709242ce330c5bb8f90a19ac64
Emily Le <emilyle265@gmail.com>
2016-10-25 10:14:34 -0700

    Update media.py
    added comments

--- media.py
+++ media.py
@@ -1,5 +1,10 @@
+# class instantiating section:
+
 class Movie():
     def __init__(self, title, poster_image, trailer):
+    
+    # Defining the attributes of the instance we're creating:
+    
         self.title = title
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer
